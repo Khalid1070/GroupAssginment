53f18df573ad30672c14d6490624faf92fbf3ba5
MUNEEB MUSLIM AL TALAQ <s202367270@kfupm.edu.sa>
2026-04-06 22:02:00 +0300

    Login. Done

--- GroupAssginment/Login.Designer.cs
+++ GroupAssginment/Login.Designer.cs
@@ -29,62 +29,112 @@ namespace GroupAssginment
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
-            this.btnStudent = new System.Windows.Forms.Button();
-            this.btnFaculty = new System.Windows.Forms.Button();
+            this.textBox_userName = new System.Windows.Forms.TextBox();
+            this.textBox_pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_LogIn = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            //
+            // 
             // lblTitle
-            //
+            // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.Location = new System.Drawing.Point(200, 50);
+            this.lblTitle.Location = new System.Drawing.Point(140, 67);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(370, 31);
+            this.lblTitle.Size = new System.Drawing.Size(444, 37);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Student Grade Management";
-            //
-            // btnStudent
-            //
-            this.btnStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btnStudent.Location = new System.Drawing.Point(180, 140);
-            this.btnStudent.Name = "btnStudent";
-            this.btnStudent.Size = new System.Drawing.Size(200, 50);
-            this.btnStudent.TabIndex = 1;
-            this.btnStudent.Text = "Student";
-            this.btnStudent.UseVisualStyleBackColor = true;
-            this.btnStudent.Click += new System.EventHandler(this.btnStudent_Click);
-            //
-            // btnFaculty
-            //
-            this.btnFaculty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btnFaculty.Location = new System.Drawing.Point(420, 140);
-            this.btnFaculty.Name = "btnFaculty";
-            this.btnFaculty.Size = new System.Drawing.Size(200, 50);
-            this.btnFaculty.TabIndex = 2;
-            this.btnFaculty.Text = "Faculty";
-            this.btnFaculty.UseVisualStyleBackColor = true;
-            this.btnFaculty.Click += new System.EventHandler(this.btnFaculty_Click);
-            //
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // textBox_userName
+            // 
+            this.textBox_userName.Location = new System.Drawing.Point(134, 201);
+            this.textBox_userName.Name = "textBox_userName";
+            this.textBox_userName.Size = new System.Drawing.Size(459, 27);
+            this.textBox_userName.TabIndex = 3;
+            // 
+            // textBox_pass
+            // 
+            this.textBox_pass.Location = new System.Drawing.Point(134, 281);
+            this.textBox_pass.Name = "textBox_pass";
+            this.textBox_pass.Size = new System.Drawing.Size(459, 27);
+            this.textBox_pass.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 38);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "User Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(140, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 38);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // button_LogIn
+            // 
+            this.button_LogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button_LogIn.Location = new System.Drawing.Point(168, 404);
+            this.button_LogIn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_LogIn.Name = "button_LogIn";
+            this.button_LogIn.Size = new System.Drawing.Size(189, 59);
+            this.button_LogIn.TabIndex = 7;
+            this.button_LogIn.Text = "Log In";
+            this.button_LogIn.UseVisualStyleBackColor = true;
+            this.button_LogIn.Click += new System.EventHandler(this.button_LogIn_Click);
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button_Exit.Location = new System.Drawing.Point(363, 404);
+            this.button_Exit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(189, 59);
+            this.button_Exit.TabIndex = 8;
+            this.button_Exit.Text = "Exit";
+            this.button_Exit.UseVisualStyleBackColor = true;
+            this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            // 
             // Form1
-            //
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 280);
-            this.Controls.Add(this.btnFaculty);
-            this.Controls.Add(this.btnStudent);
+            this.ClientSize = new System.Drawing.Size(730, 589);
+            this.Controls.Add(this.button_Exit);
+            this.Controls.Add(this.button_LogIn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_pass);
+            this.Controls.Add(this.textBox_userName);
             this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Button btnStudent;
-        private System.Windows.Forms.Button btnFaculty;
+        private System.Windows.Forms.TextBox textBox_userName;
+        private System.Windows.Forms.TextBox textBox_pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_LogIn;
+        private System.Windows.Forms.Button button_Exit;
     }
 }
--- GroupAssginment/Login.cs
+++ GroupAssginment/Login.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace GroupAssginment
@@ -15,18 +16,88 @@ namespace GroupAssginment
 
         }
 
-        private void btnStudent_Click(object sender, EventArgs e)
+       
+        private void lblTitle_Click(object sender, EventArgs e)
         {
-            Student studentForm = new Student();
-            studentForm.Show();
-            this.Hide();
+
         }
 
-        private void btnFaculty_Click(object sender, EventArgs e)
+      
+        private void label1_Click(object sender, EventArgs e)
         {
-            Faculty facultyForm = new Faculty();
-            facultyForm.Show();
-            this.Hide();
+
+        }
+
+        private void button_LogIn_Click(object sender, EventArgs e)
+        {
+            //read username and password from textboxes
+            string username = textBox_userName.Text.Trim();
+            string password = textBox_pass.Text.Trim();
+
+            //check if users.txt file exists
+            if (!File.Exists("users.txt"))
+            {
+                MessageBox.Show("users.txt file not found.");
+                return;
+            }
+
+            bool found = false;
+
+            //read users.txt file line by line
+            StreamReader inputFile = File.OpenText("users.txt");
+
+            while (!inputFile.EndOfStream)
+            {
+                string line = inputFile.ReadLine();
+                string[] parts = line.Split(',');
+
+                //check if line has 3 parts (username, password, job)
+                if (parts.Length == 3)
+                {
+                    string fileUser = parts[0];
+                    string filePass = parts[1];
+                    string job = parts[2];
+
+                    //compare with input username and password
+                    if (username == fileUser && password == filePass)
+                    {
+                        found = true;
+                        if (job == "Student")
+                        {
+                            Student sForm = new Student();
+                            sForm.ShowDialog();
+                        }
+                        else if (job == "Faculty")
+                        {
+                            Faculty fForm = new Faculty();
+                            fForm.ShowDialog();
+                        }
+
+                        break;
+                    }
+                    else 
+                    {
+                        MessageBox.Show("Invalid username or password.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid line format in users.txt" );
+                }
+            }
+
+            inputFile.Close();
+
+            if (!found)
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
+    
 }
