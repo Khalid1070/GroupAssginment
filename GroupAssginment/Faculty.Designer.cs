c7380c0fa1a7549ea4294ab1925699c69171e542
Anas-Ben10 <anaspsn03@gmail.com>
2026-04-06 18:55:18 +0300

    Faculty

---
+++ GroupAssginment/Faculty.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace GroupAssginment
+{
+    partial class Faculty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.returnToLoginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblEnterID = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblStudentResult = new System.Windows.Forms.Label();
+            this.lblCourse1 = new System.Windows.Forms.Label();
+            this.lblCourse2 = new System.Windows.Forms.Label();
+            this.lblCourse3 = new System.Windows.Forms.Label();
+            this.lblCourse4 = new System.Windows.Forms.Label();
+            this.lblCourse5 = new System.Windows.Forms.Label();
+            this.lblGPA = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.returnToLoginToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // returnToLoginToolStripMenuItem
+            // 
+            this.returnToLoginToolStripMenuItem.Name = "returnToLoginToolStripMenuItem";
+            this.returnToLoginToolStripMenuItem.Size = new System.Drawing.Size(190, 26);
+            this.returnToLoginToolStripMenuItem.Text = "Return to Login";
+            this.returnToLoginToolStripMenuItem.Click += new System.EventHandler(this.returnToLoginToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(244, 45);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(303, 29);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Faculty Grade Search Form";
+            // 
+            // lblEnterID
+            // 
+            this.lblEnterID.AutoSize = true;
+            this.lblEnterID.Location = new System.Drawing.Point(92, 106);
+            this.lblEnterID.Name = "lblEnterID";
+            this.lblEnterID.Size = new System.Drawing.Size(105, 16);
+            this.lblEnterID.TabIndex = 2;
+            this.lblEnterID.Text = "Enter Student ID:";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(216, 103);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(186, 22);
+            this.txtStudentID.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(430, 100);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 30);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(548, 100);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 30);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblStudentResult
+            // 
+            this.lblStudentResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStudentResult.Location = new System.Drawing.Point(95, 160);
+            this.lblStudentResult.Name = "lblStudentResult";
+            this.lblStudentResult.Size = new System.Drawing.Size(553, 28);
+            this.lblStudentResult.TabIndex = 6;
+            this.lblStudentResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCourse1
+            // 
+            this.lblCourse1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCourse1.Location = new System.Drawing.Point(95, 208);
+            this.lblCourse1.Name = "lblCourse1";
+            this.lblCourse1.Size = new System.Drawing.Size(553, 28);
+            this.lblCourse1.TabIndex = 7;
+            this.lblCourse1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCourse2
+            // 
+            this.lblCourse2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCourse2.Location = new System.Drawing.Point(95, 247);
+            this.lblCourse2.Name = "lblCourse2";
+            this.lblCourse2.Size = new System.Drawing.Size(553, 28);
+            this.lblCourse2.TabIndex = 8;
+            this.lblCourse2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCourse3
+            // 
+            this.lblCourse3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCourse3.Location = new System.Drawing.Point(95, 286);
+            this.lblCourse3.Name = "lblCourse3";
+            this.lblCourse3.Size = new System.Drawing.Size(553, 28);
+            this.lblCourse3.TabIndex = 9;
+            this.lblCourse3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCourse4
+            // 
+            this.lblCourse4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCourse4.Location = new System.Drawing.Point(95, 325);
+            this.lblCourse4.Name = "lblCourse4";
+            this.lblCourse4.Size = new System.Drawing.Size(553, 28);
+            this.lblCourse4.TabIndex = 10;
+            this.lblCourse4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCourse5
+            // 
+            this.lblCourse5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCourse5.Location = new System.Drawing.Point(95, 364);
+            this.lblCourse5.Name = "lblCourse5";
+            this.lblCourse5.Size = new System.Drawing.Size(553, 28);
+            this.lblCourse5.TabIndex = 11;
+            this.lblCourse5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblGPA
+            // 
+            this.lblGPA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGPA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblGPA.Location = new System.Drawing.Point(95, 406);
+            this.lblGPA.Name = "lblGPA";
+            this.lblGPA.Size = new System.Drawing.Size(553, 32);
+            this.lblGPA.TabIndex = 12;
+            this.lblGPA.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Faculty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 470);
+            this.Controls.Add(this.lblGPA);
+            this.Controls.Add(this.lblCourse5);
+            this.Controls.Add(this.lblCourse4);
+            this.Controls.Add(this.lblCourse3);
+            this.Controls.Add(this.lblCourse2);
+            this.Controls.Add(this.lblCourse1);
+            this.Controls.Add(this.lblStudentResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.lblEnterID);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Faculty";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Faculty Form";
+            this.Load += new System.EventHandler(this.Faculty_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem returnToLoginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblEnterID;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblStudentResult;
+        private System.Windows.Forms.Label lblCourse1;
+        private System.Windows.Forms.Label lblCourse2;
+        private System.Windows.Forms.Label lblCourse3;
+        private System.Windows.Forms.Label lblCourse4;
+        private System.Windows.Forms.Label lblCourse5;
+        private System.Windows.Forms.Label lblGPA;
+    }
+}
---
+++ GroupAssginment/Faculty.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GroupAssginment
+{
+    public partial class Faculty : Form
+    {
+        // 1D array for course names
+        private string[] courseNames = { "MIS302", "ICS104", "MATH101", "IAS212", "ENG102" };
+
+        // 1D array for student IDs
+        private string[] studentIds = { "20210001", "20210002", "20210003" };
+
+        // 2D array for grades of multiple students in multiple courses
+        private string[,] studentGrades =
+        {
+            { "A", "B", "C", "A", "B" },
+            { "B", "C", "B", "B", "A" },
+            { "A", "A", "B", "A", "A" }
+        };
+
+        public Faculty()
+        {
+            InitializeComponent();
+        }
+
+        private void Faculty_Load(object sender, EventArgs e)
+        {
+            ClearOutput();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchId = txtStudentID.Text.Trim();
+            int studentIndex;
+
+            if (searchId == "")
+            {
+                MessageBox.Show("Please enter a student ID.");
+                txtStudentID.Focus();
+            }
+            else
+            {
+                studentIndex = FindStudent(searchId);
+
+                if (studentIndex == -1)
+                {
+                    MessageBox.Show("Student ID not found.");
+                    ClearOutput();
+                    txtStudentID.Focus();
+                }
+                else
+                {
+                    DisplayStudentData(studentIndex);
+                }
+            }
+        }
+
+        private int FindStudent(string id)
+        {
+            for (int i = 0; i < studentIds.Length; i++)
+            {
+                if (studentIds[i] == id)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private void DisplayStudentData(int row)
+        {
+            lblStudentResult.Text = "Student ID: " + studentIds[row];
+
+            lblCourse1.Text = courseNames[0] + ": " + studentGrades[row, 0];
+            lblCourse2.Text = courseNames[1] + ": " + studentGrades[row, 1];
+            lblCourse3.Text = courseNames[2] + ": " + studentGrades[row, 2];
+            lblCourse4.Text = courseNames[3] + ": " + studentGrades[row, 3];
+            lblCourse5.Text = courseNames[4] + ": " + studentGrades[row, 4];
+
+            lblGPA.Text = "GPA: " + CalculateGPA(row).ToString("F2");
+        }
+
+        private double CalculateGPA(int row)
+        {
+            double totalPoints = 0;
+
+            for (int col = 0; col < studentGrades.GetLength(1); col++)
+            {
+                totalPoints += GetGradePoints(studentGrades[row, col]);
+            }
+
+            return totalPoints / studentGrades.GetLength(1);
+        }
+
+        private int GetGradePoints(string grade)
+        {
+            if (grade == "A")
+            {
+                return 4;
+            }
+            else if (grade == "B")
+            {
+                return 3;
+            }
+            else if (grade == "C")
+            {
+                return 2;
+            }
+            else if (grade == "D")
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void ClearOutput()
+        {
+            lblStudentResult.Text = "";
+            lblCourse1.Text = "";
+            lblCourse2.Text = "";
+            lblCourse3.Text = "";
+            lblCourse4.Text = "";
+            lblCourse5.Text = "";
+            lblGPA.Text = "";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStudentID.Text = "";
+            ClearOutput();
+            txtStudentID.Focus();
+        }
+
+        private void returnToLoginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 loginForm = new Form1();
+            loginForm.Show();
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Team Members:\nYour Name - Your ID\nMember 2 - ID\nMember 3 - ID",
+                            "About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
