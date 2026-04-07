04c4f5f40e806cc1058c8a2e18ae6ba11e5e5c68
Anas-Ben10 <anaspsn03@gmail.com>
2026-04-07 10:13:26 +0300

    Faculty

--- GroupAssginment/Login.Designer.cs
+++ GroupAssginment/Login.Designer.cs
@@ -38,27 +38,26 @@ namespace GroupAssginment
             // 
             // textBox_userName
             // 
-            this.textBox_userName.Location = new System.Drawing.Point(92, 68);
-            this.textBox_userName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_userName.Location = new System.Drawing.Point(123, 84);
+            this.textBox_userName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_userName.Name = "textBox_userName";
-            this.textBox_userName.Size = new System.Drawing.Size(307, 20);
+            this.textBox_userName.Size = new System.Drawing.Size(408, 22);
             this.textBox_userName.TabIndex = 3;
             // 
             // textBox_pass
             // 
-            this.textBox_pass.Location = new System.Drawing.Point(92, 127);
-            this.textBox_pass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_pass.Location = new System.Drawing.Point(123, 156);
+            this.textBox_pass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_pass.Name = "textBox_pass";
-            this.textBox_pass.Size = new System.Drawing.Size(307, 20);
+            this.textBox_pass.Size = new System.Drawing.Size(408, 22);
             this.textBox_pass.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(92, 34);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(123, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 31);
+            this.label1.Size = new System.Drawing.Size(245, 38);
             this.label1.TabIndex = 5;
             this.label1.Text = "User Name";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -66,20 +65,19 @@ namespace GroupAssginment
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(92, 95);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(123, 117);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 30);
+            this.label2.Size = new System.Drawing.Size(178, 37);
             this.label2.TabIndex = 6;
             this.label2.Text = "Password";
             // 
             // button_LogIn
             // 
             this.button_LogIn.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_LogIn.Location = new System.Drawing.Point(115, 206);
-            this.button_LogIn.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button_LogIn.Location = new System.Drawing.Point(153, 254);
+            this.button_LogIn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button_LogIn.Name = "button_LogIn";
-            this.button_LogIn.Size = new System.Drawing.Size(126, 40);
+            this.button_LogIn.Size = new System.Drawing.Size(168, 49);
             this.button_LogIn.TabIndex = 7;
             this.button_LogIn.Text = "Log In";
             this.button_LogIn.UseVisualStyleBackColor = true;
@@ -88,10 +86,10 @@ namespace GroupAssginment
             // button_Exit
             // 
             this.button_Exit.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_Exit.Location = new System.Drawing.Point(245, 206);
-            this.button_Exit.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button_Exit.Location = new System.Drawing.Point(327, 254);
+            this.button_Exit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button_Exit.Name = "button_Exit";
-            this.button_Exit.Size = new System.Drawing.Size(126, 40);
+            this.button_Exit.Size = new System.Drawing.Size(168, 49);
             this.button_Exit.TabIndex = 8;
             this.button_Exit.Text = "Exit";
             this.button_Exit.UseVisualStyleBackColor = true;
@@ -99,16 +97,16 @@ namespace GroupAssginment
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 403);
+            this.ClientSize = new System.Drawing.Size(649, 496);
             this.Controls.Add(this.button_Exit);
             this.Controls.Add(this.button_LogIn);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox_pass);
             this.Controls.Add(this.textBox_userName);
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
